26cc155e9724a408d96ab67ea760ff300484c019
Loic <llh1@sanger.ac.uk>
2013-02-11 15:37:18 +0000

    add transfer tubes to tubes resource

--- spec/integrations/tube_spec.rb
+++ spec/integrations/tube_spec.rb
@@ -67,4 +67,112 @@ describe Lims::Core::Laboratory::Tube do
       it_behaves_like('creating a resource with a label on it')
     end
   end
+
+  context "#transfer tubes to tubes", :focus  => true do
+    let(:url) { "/actions/transfer_tubes_to_tubes" }
+    context "with empty tubes" do
+      let(:parameters) { { :transfer_tubes_to_tubes => {} } }
+      let(:expected_json) { {"errors" => {:transfers => "invalid" }
+      }}
+      it_behaves_like "an invalid core action", 422  # Unprocessable entity
+    end
+
+    context "from a tube with samples" do
+      include_context "with filled aliquots"
+      context "to an existing target tube" do
+        let(:unit_type) { "mole" }
+        let(:aliquot_type) { "NA" }
+        let(:source_tube1_uuid) { '22222222-3333-4444-1111-000000000000'.tap do |uuid|
+          quantity = 100
+          volume = 100
+          store.with_session do |session|
+            tube = Lims::Core::Laboratory::Tube.new
+            L=Lims::Core::Laboratory
+            (1..1).each do |i|
+              sample = L::Sample.new(:name => "Sample ##{i}")
+              aliquot = L::Aliquot.new(:sample => sample, :quantity => quantity)
+              tube << aliquot
+            end
+            tube << L::Aliquot.new(:type => L::Aliquot::Solvent, :quantity => volume)
+
+            session << tube
+            set_uuid(session, tube, uuid)
+          end
+        end
+
+        }
+        let(:target_tube2_uuid) { '22222222-3333-4444-1111-000000000001'.tap do |uuid|
+            store.with_session do |session|
+              tube = Lims::Core::Laboratory::Tube.new
+              
+              session << tube
+              set_uuid(session, tube, uuid)
+            end
+          end
+        }
+        let(:transfers) { [ { "source_uuid" => source_tube1_uuid,
+                              "target_uuid" => target_tube2_uuid,
+                              "amount" => 100,
+                              "aliquot_type" => "NA"}
+          ]
+        }
+        let(:parameters) { { :transfer_tubes_to_tubes => { :transfers => transfers} }}
+        let(:sample_uuid) { '11111111-2222-3333-4444-888888888888' }
+        let(:sample_name) { "sample 1" }
+
+        let(:expected_json) {
+          source_tube1_url = "http://example.org/#{source_tube1_uuid}"
+          target_tube2_url = "http://example.org/#{target_tube2_uuid}"
+          { :transfer_tubes_to_tubes => 
+            { :actions => {},
+              :user => "user",
+              :application => "application",
+              "result"=> {
+                "tubes" => [
+                  {"tube" => {
+                    "actions" => {
+                      "create" => source_tube1_url,
+                      "read" => source_tube1_url,
+                      "update" => source_tube1_url,
+                      "delete" => source_tube1_url
+                    },
+                    "uuid" => source_tube1_uuid,
+                    "aliquots" => aliquot_array
+                  }}
+                ]},
+                "targets" => {"tubes" => [
+                  {"tube" => {
+                    "actions" => {
+                      "create" => target_tube2_url,
+                      "read" => target_tube2_url,
+                      "update" => target_tube2_url,
+                      "delete" => target_tube2_url
+                    },
+                    "uuid" => target_tube2_uuid,
+                    "aliquots" => aliquot_array
+                  }}
+                ]
+              },
+                 "sources" => {"tubes" => [
+                  {"tube" => {
+                    "actions" => {
+                      "create" => target_tube2_url,
+                      "read" => target_tube2_url,
+                      "update" => target_tube2_url,
+                      "delete" => target_tube2_url
+                    },
+                    "uuid" => target_tube2_uuid,
+                    "aliquots" => aliquot_array
+                  }}
+                ]
+              },
+              "transfers" => transfers
+            }
+          }
+        }
+        
+        it_behaves_like "a valid core action"
+      end
+    end
+  end
 end
